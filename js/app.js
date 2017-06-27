33edc603e38dc7c53595396417aae569b03b5a1e
Breidbart <ebreidbart@gmail.com>
2017-06-27 16:35:26 -0400

    dom ready

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
-new Vivus('jg', {type: 'async', duration: 200, start: 'inViewport'}, function(obj) {
-  obj.el.classList.add('done');
+DomReady.ready(function() { 
+  new Vivus('jg', {type: 'async', duration: 200, start: 'inViewport'}, function(obj) {
+    obj.el.classList.add('done');
+  });
 });
