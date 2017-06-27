2f7fb0ac2706a0c03436fee5400f9c62300e7d45
Breidbart <ebreidbart@gmail.com>
2017-06-27 16:46:01 -0400

    vivus change

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-DomReady.ready(function() { 
-  new Vivus('jg', {type: 'async', duration: 200, start: 'inViewport'}, function(obj) {
+DomReady.ready(function() {
+  new Vivus('jg', {type: 'async', duration: 200, start: 'autostart'}, function(obj) {
     obj.el.classList.add('done');
   });
 });
