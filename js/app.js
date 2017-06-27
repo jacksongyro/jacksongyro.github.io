df993c3cc768e20ea74ab6565427292407fd5beb
breidbart <ebreidbart@gmail.com>
2017-06-27 19:49:10 -0400

    animation and social

--- js/app.js
+++ js/app.js
@@ -2,4 +2,8 @@ DomReady.ready(function() {
   new Vivus('jg', {type: 'async', duration: 200, start: 'autostart'}, function(obj) {
     obj.el.classList.add('done');
   });
+
+  document.querySelector('h1.comingsoon').style.top = '0';
+  document.querySelector('h1.address').style.opacity = '1';
+  document.querySelector('.social').style.bottom = '0';
 });
