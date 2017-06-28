bf7d522b31f7cfe3b23d2ae13c88e9ec48852a4f
breidbart <ebreidbart@gmail.com>
2017-06-27 20:51:49 -0400

    fix animations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,46 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const animatedObjects = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+  animatedObjects.forEach(sliderImage => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.clientHeight / 2);
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.clientHeight;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+          sliderImage.classList.remove('active');
+        }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
+
 DomReady.ready(function() {
   new Vivus('jg', {type: 'async', duration: 200, start: 'autostart'}, function(obj) {
     obj.el.classList.add('done');
   });
 
-  document.querySelector('h1.comingsoon').style.top = '0';
-  document.querySelector('h1.address').style.opacity = '1';
-  document.querySelector('.social').style.bottom = '0';
+  setTimeout(function(){
+    document.querySelector('h1.comingsoon').style.top = '0';
+    document.querySelector('h1.address').style.opacity = '1';
+  }, 500);
+  
 });
